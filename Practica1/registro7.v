4abcd6faa074bf5bc20a856946da19f0780f9b46
Jose Rocabado <jose_1298@hotmail.com>
2021-10-25 17:46:49 +0200

    Comentado, posible código por si el actual no sea correcto

--- Practica1/registro7.v
+++ Practica1/registro7.v
@@ -1,16 +1,20 @@
 module registro7 (iCLK, iRST_n, iENABLE, iSR, oPR, oSR);
 	input iCLK, iRST_n, iENABLE, iSR;
 	output reg [6:0] oPR;
+	//output reg oSR; //option B
 	output oSR;
 	always @ (posedge iCLK)
 		begin
 			if ( !iRST_n )
-					oPR <= 0;							
+					oPR <= 0;
+					//oSR <= 0;							
 			else if ( iENABLE )
 				begin
+				//oSR <= oPR[0];
 				oPR[5:0] <= oPR[6:1];
 				oPR[6] <= iSR;
 				end
 		end
+	//comment this in case we need option B
 	assign oSR = (iRST_n)?oPR[0]:0;
 endmodule
