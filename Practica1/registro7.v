ce6092b578dd668a0bf4c745077a0d4c8955d125
Jose Rocabado <jose_1298@hotmail.com>
2021-10-26 17:36:36 +0200

    Codigo con la simulación con ModelSim

--- Practica1/registro7.v
+++ Practica1/registro7.v
@@ -1,20 +1,22 @@
 module registro7 (iCLK, iRST_n, iENABLE, iSR, oPR, oSR);
 	input iCLK, iRST_n, iENABLE, iSR;
 	output reg [6:0] oPR;
-	//output reg oSR; //option B
-	output oSR;
+	output reg oSR; //option B
+	//output oSR; //option A
 	always @ (posedge iCLK)
 		begin
 			if ( !iRST_n )
+				begin
 					oPR <= 0;
-					//oSR <= 0;							
+					oSR <= 0;
+				end							
 			else if ( iENABLE )
 				begin
-				//oSR <= oPR[0];
-				oPR[5:0] <= oPR[6:1];
-				oPR[6] <= iSR;
+					oSR <= oPR[0];
+					oPR[5:0] <= oPR[6:1];
+					oPR[6] <= iSR;
 				end
 		end
 	//comment this in case we need option B
-	assign oSR = (iRST_n)?oPR[0]:0;
+	//assign oSR = (iRST_n)?oPR[0]:0;
 endmodule
