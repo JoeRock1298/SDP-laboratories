9983c6d95adebc51c3d645d1ead15bb74c1cf952
Jose Rocabado <jose_1298@hotmail.com>
2021-10-26 18:54:28 +0200

    ModelSim funcional, previo a la modificación del TestBench

--- Practica1/registro7.v
+++ Practica1/registro7.v
@@ -1,22 +1,20 @@
 module registro7 (iCLK, iRST_n, iENABLE, iSR, oPR, oSR);
 	input iCLK, iRST_n, iENABLE, iSR;
-	output reg [6:0] oPR;
-	output reg oSR; //option B
-	//output oSR; //option A
+	output [6:0] oPR;
+	output oSR; 
+	reg [6:0] out_pivot;
 	always @ (posedge iCLK)
 		begin
 			if ( !iRST_n )
 				begin
-					oPR <= 0;
-					oSR <= 0;
+					out_pivot <= 0;
 				end							
 			else if ( iENABLE )
 				begin
-					oSR <= oPR[0];
-					oPR[5:0] <= oPR[6:1];
-					oPR[6] <= iSR;
+					out_pivot[5:0] <= out_pivot[6:1];
+					out_pivot[6] <= iSR;
 				end
 		end
-	//comment this in case we need option B
-	//assign oSR = (iRST_n)?oPR[0]:0;
+	assign oSR = out_pivot[0];
+	assign oPR = out_pivot;
 endmodule
